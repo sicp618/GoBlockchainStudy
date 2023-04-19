aea542cf1188e149cca5bb1118a3ef3625f551d4
aaron <sicp618@gmail.com>
2023-04-19 19:17:41 +0800

    eth account

--- goeth/02/account.go
+++ goeth/02/account.go
@@ -3,11 +3,16 @@ package main
 import (
 	"context"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"math"
 	"math/big"
+	"os"
 
+	"github.com/ethereum/go-ethereum/accounts/keystore"
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/ethclient"
 )
 
@@ -18,6 +23,7 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	defer client.Close()
 
 	account := common.HexToAddress(addr)
 	balance, err := client.BalanceAt(context.Background(), account, nil)
@@ -40,4 +46,43 @@ func main() {
 
 	pendingBalance, err := client.PendingBalanceAt(context.Background(), account)
 	fmt.Println(pendingBalance)
+
+	privateKey, err := crypto.GenerateKey()
+	privateKeyBytes := crypto.FromECDSA(privateKey)
+	fmt.Println(hexutil.Encode(privateKeyBytes))
+
+	createKs()
+}
+
+
+func createKs() {
+    ks := keystore.NewKeyStore("./tmp", keystore.StandardScryptN, keystore.StandardScryptP)
+    password := "secret"
+    account, err := ks.NewAccount(password)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    fmt.Println(account.Address.Hex()) // 0x20F8D42FB0F667F2E53930fed426f225752453b3
+}
+
+func importKs() {
+    file := "./tmp/UTC--2018-07-04T09-58-30.122808598Z--20f8d42fb0f667f2e53930fed426f225752453b3"
+    ks := keystore.NewKeyStore("./tmp", keystore.StandardScryptN, keystore.StandardScryptP)
+    jsonBytes, err := ioutil.ReadFile(file)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    password := "secret"
+    account, err := ks.Import(jsonBytes, password, password)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    fmt.Println(account.Address.Hex()) // 0x20F8D42FB0F667F2E53930fed426f225752453b3
+
+    if err := os.Remove(file); err != nil {
+        log.Fatal(err)
+    }
 }
